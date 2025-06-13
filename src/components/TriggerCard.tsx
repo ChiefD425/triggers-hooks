ec9ebdd58bea80f9c18bcb0e7eba8aec1f74cbd0
[dyad] <fred.deichler@gmail.com>
2025-06-12 17:10:04 -0700

    [dyad] Fixed Trigger card border visibility - wrote 1 file(s)

--- src/components/TriggerCard.tsx
+++ src/components/TriggerCard.tsx
@@ -14,18 +14,17 @@ const TriggerCard: React.FC<TriggerCardProps> = ({ id, text, categoryName, color
   return (
     <Card
       className={cn(
-        "w-48 h-64 flex flex-col justify-between text-white cursor-pointer transition-transform transform hover:scale-105 border-6 border-black",
-        color
+        "w-48 h-64 flex flex-col justify-between text-black cursor-pointer transition-transform transform hover:scale-105 border-6 border-black bg-white", // Main card background is white, border is black
       )}
       onClick={onClick}
     >
-      <CardHeader className="p-3 pb-0">
+      <CardHeader className={cn("p-3 pb-0", color)}> {/* Apply category color to header */}
         <CardTitle className="text-sm font-semibold text-center text-white">Trigger</CardTitle>
       </CardHeader>
       <CardContent className="flex-grow flex flex-col items-center justify-center p-3 text-center">
         <p className="text-lg font-bold text-black">{text}</p>
       </CardContent>
-      <div className="p-2 text-xs text-center text-gray-100 italic">
+      <div className="p-2 text-xs text-center text-gray-700 italic"> {/* Changed text color for readability */}
         {categoryName}
       </div>
     </Card>
